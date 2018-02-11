f04bc137b84a28b1aa3f662c40aeddb993f868f1
rbryan21 <rbryan21@gmail.com>
2018-02-11 14:25:17 -0500

    adding angular material, angular flexbox, browser polyfills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 
@@ -12,7 +13,8 @@ import {AppRoutingModule} from "./app-routing.module";
     AppRoutingModule.routingComponents
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
