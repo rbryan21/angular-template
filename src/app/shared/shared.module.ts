265c1eb6119f02f4e239a6235d0573ad9556432c
rbryan21 <rbryan21@gmail.com>
2018-02-11 13:45:10 -0500

    Adding generated angular-cli project. Adding routing module to app module, core and common modules.

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+/**
+ * This module should declare common components, directives, and pipes that are used throughout the application.
+ * Avoid providing services that you wish to be singletons.
+ */
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class SharedModule { }
